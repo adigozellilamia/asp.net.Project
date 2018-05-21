83ac0f3aa1cf4e1dff651d6010929246fa9ffd0b
= <=>
2018-05-22 00:00:58 +0400

    asp.netProject

---
+++ CV/Areas/Admin/Controllers/TextsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CV.Models;
+
+namespace CV.Areas.Admin.Controllers
+{
+    public class TextsController : Controller
+    {
+        private CvEntities2 db = new CvEntities2();
+
+        // GET: Admin/Texts
+        public ActionResult Index()
+        {
+            return View(db.Texts.ToList());
+        }
+
+        // GET: Admin/Texts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Text text = db.Texts.Find(id);
+            if (text == null)
+            {
+                return HttpNotFound();
+            }
+            return View(text);
+        }
+
+        // GET: Admin/Texts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Texts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,Tittle,Title_content,Text1,Page,Start_date,End_date")] Text text)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Texts.Add(text);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(text);
+        }
+
+        // GET: Admin/Texts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Text text = db.Texts.Find(id);
+            if (text == null)
+            {
+                return HttpNotFound();
+            }
+            return View(text);
+        }
+
+        // POST: Admin/Texts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,Tittle,Title_content,Text1,Page,Start_date,End_date")] Text text)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(text).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(text);
+        }
+
+        // GET: Admin/Texts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Text text = db.Texts.Find(id);
+            if (text == null)
+            {
+                return HttpNotFound();
+            }
+            return View(text);
+        }
+
+        // POST: Admin/Texts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Text text = db.Texts.Find(id);
+            db.Texts.Remove(text);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CV/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CV.Models;
+
+namespace CV.Controllers
+{
+    public class HomeController : Controller
+    {
+        CvEntities2 db = new CvEntities2();
+        public ActionResult Index()
+        {
+            ViewBag.Abouts = db.Abouts.ToList();
+            
+            
+            ViewBag.Skills_icon = db.Icons.Where(w => w.Page == "Skills").ToList();
+            ViewBag.Skills_list = db.Lists.Where(w => w.Page == "Skills").ToList();
+            ViewBag.Awards_list = db.Lists.Where(w => w.Page == "Awards").ToList();
+            ViewBag.Experience_text = db.Texts.Where(w => w.Page == "Experience").ToList();
+            ViewBag.Educatione_text = db.Texts.Where(w => w.Page == "Education").ToList();
+            ViewBag.Interests_text = db.Texts.Where(w => w.Page == "Interests").ToList();
+            return View();
+        }
+
+        
+
+    }
+}
